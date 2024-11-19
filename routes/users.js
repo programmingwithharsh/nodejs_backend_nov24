06c147c43f3e459fbf7c7d13e72f466f02fe44b5
Harsh Tiwari <harshtiwari343@email.com>
2024-11-19 10:32:29 +0530

    Added GET, POST, PUT and DELETE APIs

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  let users = [
+    {
+      "id": 1,
+      "name": "text user",
+      "email": "text@gmail.com"
+    },
+    {
+      "id": 2,
+      "name": "anne hunter",
+      "email": "anne@gmail.com"
+    },
+    {
+      "id": 3,
+      "name": "jale boser",
+      "email": "jale@gmail.com"
+    }
+  ];
+  res.send(users);
 });
+
+// POST API
+router.post('/', (req, res) => {
+  res.send('POST request')
+})
+
+// PUT API
+router.put('/', (req, res) => {
+  res.send('Update request')
+})
+
+// DELETE API
+router.delete('/', (req, res) => {
+  res.send('delete request')
+})
 
 module.exports = router;
